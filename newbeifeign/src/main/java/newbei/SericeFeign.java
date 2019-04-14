d0a548d929648811119c78860f3ef8720df0d253
hegang <hegang@zetyun.com>
2019-04-15 00:50:51 +0800

    finish eureka\client\feign

---
+++ newbeifeign/src/main/java/newbei/SericeFeign.java
@@ -0,0 +1,17 @@
+package newbei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SericeFeign {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SericeFeign.class, args);
+    }
+
+}
